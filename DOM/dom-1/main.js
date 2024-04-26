b928731a676741f3647b1f90314d06e9f472a2ad
sinemablak <87177874+sinemablak@users.noreply.github.com>
2024-04-26 19:26:44 +0300

    Delete DOM directory

--- DOM/dom-1/main.js
+++
@@ -1,9 +0,0 @@
-//Baslik elementi secme
-const baslikElementi = document.getElementById("baslik");
-//Buton elementi secme
-const degistirBtn = document.getElementById("degistir");
-
-//Butona tiklaninca metin degistirme
-degistirBtn.addEventListener("click", function () {
-  baslikElementi.textContent = "Butona tikladin :)";
-});
